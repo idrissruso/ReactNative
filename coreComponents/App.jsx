b0ba838b94a80066761ea970cf07d7dc81710129
Idrissa <idrisabkar2018@gmail.com>
2024-04-08 22:14:17 +0300

    core components

--- coreComponents/App.jsx
+++ coreComponents/App.jsx
@@ -1,11 +1,40 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, Pressable, Modal } from 'react-native'
+import { useState } from 'react'
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false)
   return (
-    <View style={{ backgroundColor: 'plum', flex: 1, padding: 20 }}>
+    <ImageBackground
+      source={{ uri: 'https://picsum.photos/1000' }}
+      style={{ backgroundColor: 'plum', flex: 1, padding: 20 }}
+    >
       <View style={{ backgroundColor: 'lightblue', width: 100, height: 100 }}>
-        <Text>hello world</Text>
+        <Pressable
+          onPress={() => {
+            setModalVisible(true)
+            console.log('pressed the btn')
+          }}
+        >
+          <Text>Press me</Text>
+        </Pressable>
+        <Modal
+          visible={modalVisible}
+          animationType="slide"
+          presentationStyle="pageSheet"
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <View>
+            <Text>Modal content</Text>
+            <Pressable
+              onPress={() => {
+                setModalVisible(false)
+              }}
+            >
+              <Text>Close</Text>
+            </Pressable>
+          </View>
+        </Modal>
       </View>
-    </View>
+    </ImageBackground>
   )
 }
