23857bff30adf7a90d26cc3fcc22e13b45512b18
Idrissa <idrisabkar2018@gmail.com>
2024-04-12 15:36:58 +0300

    Refactor App.js to include a FlatList component

--- helloWorld/App.js
+++ helloWorld/App.js
@@ -1,29 +1,68 @@
-import { View, Button, Text, StyleSheet, StatusBar } from 'react-native'
+import {
+  View,
+  Button,
+  Text,
+  StyleSheet,
+  StatusBar,
+  FlatList,
+} from 'react-native'
 import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <View style={styles.container}>
-      <Text>You Pressed the Button {count} times</Text>
-      <Button
-        title="Increase"
-        onPress={() => {
-          setCount(count + 1)
-        }}
+    <View style={styles.app}>
+      <FlatList
+        data={[
+          { key: 'red', color: 'red' },
+          { key: 'blue', color: 'blue' },
+          { key: 'green', color: 'green' },
+          { key: 'red', color: 'red' },
+          { key: 'blue', color: 'blue' },
+          { key: 'green', color: 'green' },
+          { key: 'red', color: 'red' },
+          { key: 'blue', color: 'blue' },
+          { key: 'green', color: 'green' },
+          { key: 'red', color: 'red' },
+          { key: 'blue', color: 'blue' },
+          { key: 'green', color: 'green' },
+          { key: 'red', color: 'red' },
+          { key: 'blue', color: 'blue' },
+          { key: 'green', color: 'green' },
+          { key: 'red', color: 'red' },
+          { key: 'blue', color: 'blue' },
+          { key: 'green', color: 'green' },
+          { key: 'red', color: 'red' },
+          { key: 'blue', color: 'blue' },
+          { key: 'green', color: 'green' },
+        ]}
+        renderItem={({ item }) => <Box text={item.key} color={item.color} />}
       />
+
       <StatusBar />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 10,
   },
 })
+
+function Box({ text, color }) {
+  return (
+    <View
+      style={{
+        backgroundColor: color,
+        padding: 20,
+        flexGrow: 1,
+        alignItems: 'center',
+      }}
+    >
+      <Text>{text}</Text>
+    </View>
+  )
+}
 
 export default App
