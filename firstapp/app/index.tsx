f0e8817f09c5c9b83f927647681bb3653df9cc52
Idrissa <idrisabkar2018@gmail.com>
2024-09-16 22:59:16 +0300

    Refactor App.js to include styled divisions in index.tsx

--- firstapp/app/index.tsx
+++ firstapp/app/index.tsx
@@ -1,15 +1,19 @@
 import { Text, View, StyleSheet } from 'react-native'
 
 export default function index() {
-  return <Text>index</Text>
+  return (
+    <Text>
+      <Div style={styles.division} text="My Name is idrissa"></Div>
+      <Div style={styles.division} text="I am 24 yeas old"></Div>
+      <Div style={styles.division} text="My Name is idrissa"></Div>
+      <Div style={styles.division} text="Here is my X Handle X"></Div>
+      <Div style={styles.division} text="I love react"></Div>
+    </Text>
+  )
 }
 
-function Container() {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  )
+function Div({ text, style }: { text: string; style?: object }) {
+  return <View style={style}>{text}</View>
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +21,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  division: {
+    marginBottom: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 2, height: 3 },
+    shadowOpacity: 2,
+    shadowRadius: 3,
   },
 })
