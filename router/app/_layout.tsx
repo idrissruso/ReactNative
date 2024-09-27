36a59e127bad2b6ed9532dd0ccb12f4adfe4ef50
Idrissa <idrisabkar2018@gmail.com>
2024-09-27 23:23:05 +0300

    Refactor _layout.tsx to set initial route name for Stack component

--- router/app/_layout.tsx
+++ router/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router'
 const _layout = () => {
   return (
     <Stack>
-      <Stack screenOptions={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }} initialRouteName="user" />
     </Stack>
   )
 }
